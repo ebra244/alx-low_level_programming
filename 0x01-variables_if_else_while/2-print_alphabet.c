189811968837f6f9d22ae74388254b1a0c5992cd
ebra244 <ebra244@gmail.com>
2022-12-10 10:01:06 -0800

    Commit for I sometimes suffer from insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+	c++;
+	}
+	putchar('\n');
+	return (0);
+}

dd291e41927321ba3381d7a88770b3ac110ceeda
ebra244 <ebra244@gmail.com>
2022-12-11 03:21:02 -0800

    Commit for Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,12 +7,18 @@
  */
 int main(void)
 {
-	int n = 0x0;
+	int n = 0;
+	char c = 'a';
 
-	while (n <= 0xf)
+	while (n <= 9)
 	{
-		putchar(n);
+		putchar(n + '0');
 		n++;
+	}
+	while (c <= 'f')
+	{
+		putchar(c);
+		c++;
 	}
 	putchar('\n');
 	return (0);

fcbe2bb8b9543a5d8150301fb41ae550dfee362a
ebra244 <ebra244@gmail.com>
2022-12-11 03:32:32 -0800

    Commit for persistence and perspiration make an unbeatable

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+/**
+ * main - Program that prints all possible combination of single digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		putchar(n + '0');
+		if (n < 9)
+			putchar(',');
+		putchar(' ');
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}

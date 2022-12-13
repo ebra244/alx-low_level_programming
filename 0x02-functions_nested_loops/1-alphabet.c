b8a00231aa3416a30370fa585ba9e280034040fe
ebra244 <ebra244@gmail.com>
2022-12-13 10:48:43 -0800

    Commit for  I sometimes suffer from insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - Program that prints the alphabet in lower case
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}

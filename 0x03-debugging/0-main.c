c26d14c150cdfec02bb0a014e2bc3410770a8d4f
ebra244 <ebra244@gmail.com>
2022-12-14 06:22:27 -0800

    Commit for Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * 0-main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
+int 0-main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}

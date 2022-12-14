6b64a78ff94d3b3a747808965da1e84ece5b8fa9
ebra244 <ebra244@gmail.com>
2022-12-14 08:15:30 -0800

    Commit for Like, comment, subscribe

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("infinite loop incoming :\n");
+
+	i = 0;
+
+	/**
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+	printf("Infinite loop avoide! \\o/\n");
+	return (0);
+}

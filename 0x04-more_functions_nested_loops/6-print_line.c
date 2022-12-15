07475f821159a3cc32b5c54fd97d42a11d77a798
ebra244 <ebra244@gmail.com>
2022-12-15 14:06:51 -0800

    Commit for isdigit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_line - Function that draws straight line
+ * @n: Charcter to draw aline
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (n > 0)
+		{
+			
+		}
+	}
+	
+	while(c <= n)
+	{
+		_putchar('_');
+	}
+}

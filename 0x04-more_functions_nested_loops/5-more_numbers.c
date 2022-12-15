9bacf56543f93a044bb9e3283eec237f905a606c
ebra244 <ebra244@gmail.com>
2022-12-15 11:46:01 -0800

    Commit Numbers constitute the only universal language

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - Function that prints ten the numbers from 0 t0 14
+ */
+void more_numbers(void)
+{
+	int n = 0;
+
+	while (n <= 10)
+	{
+		int m = 0;
+		while (m <= 14)
+		{
+			_putchar(n + '0');
+			m++;
+		}
+		_putchar('\n');
+		n++;
+	}
+	_putchar('\n');
+}

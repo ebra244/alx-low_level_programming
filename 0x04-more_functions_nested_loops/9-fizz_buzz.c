68441db140abdd03ca3dc4179482a501db614976
ebra244 <ebra244@gmail.com>
2022-12-16 10:15:44 -0800

    Commit for FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,45 +1,31 @@
-#include "main.h"
+#include <stdio.h>
 /**
- * print_fizz_buzz - Program that prints numbers from 1 to 100
+ * main - Program that prints numbers from 1 to 100
+ * Return: Always 0 (If Successed)
  */
-void print_fizz_buzz(void)
+int main(void)
 {
 	int i = 1;
 
 	while (i <= 100)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if (i % 15 == 0)
 		{
-			_putchar(70);
-			_putchar(105);
-			_putchar(122);
-			_putchar(122);
-			_putchar(66);
-			_putchar(117);
-			_putchar(122);
-			_putchar(122);
-			_putchar(32);
+			printf("FizzBuzz ");
 		}
-		else if (i % 3 == 0 && i % 5 != 0)
+		else if (i % 3 == 0)
 		{
-			_putchar(70);
-			_putchar(105);
-			_putchar(122);
-			_putchar(122);
-			_putchar(32);
+			printf("Fizz ");
 		}
-		else if (i % 3 != 0 && i % 5 == 0)
+		else if (i % 5 == 0)
 		{
-			_putchar(66);
-			_putchar(117);
-			_putchar(122);
-			_putchar(122);
-			_putchar(32);
+			printf("Buzz ");
 		}
 		else
-			_putchar(i + '0');
+			printf("%d ", i);
 		i++;
 	}
-	_putchar('\n');
+	printf("\n");
+	return (0);
 }
 

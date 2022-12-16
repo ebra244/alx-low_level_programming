efdd4d57f55286d9a4f30879e1bea51aaf2ac872
ebra244 <ebra244@gmail.com>
2022-12-16 11:06:31 -0800

    Commit for Triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,9 +10,8 @@ void print_triangle(int size)
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = 1; j < (size - 1); j++)
+		for (j = 1; j < i; j++)
 			_putchar(' ');
-		for (j--; j < size; j++)
 			_putchar(35);
 		if (i < (size - 1))
 			_putchar('\n');

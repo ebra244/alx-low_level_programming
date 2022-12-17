8d9cd2ffe0589bbcd14dfaabddc8357c9f08877b
ebra244 <ebra244@gmail.com>
2022-12-17 07:38:23 -0800

    Commit for Numbers have life

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,5 +5,25 @@
  */
 void print_number(int n)
 {
-	_putchar(n + '0');
-}
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+		if (n > 0)
+		{
+			_putchar(n % 10 + '0');
+		}
+		else
+		{
+			_putchar(-n % 10 + '0');
+		}
+	}
+	else if ((n / 10 == 0) && (n % 10 != 0) && (n > 0))
+			{
+			_putchar(n % 10 + '0');
+			}
+			else if ((n / 10 == 0) && (n % 10 != 0) && (n <= 0))
+			{
+			_putchar('-');
+			_putchar(-n % 10 + '0');
+			}
+			}

fe05fd56ecd309a2ad8d1f6ca44a71d3d7a0f0c5
ebra244 <ebra244@gmail.com>
2022-12-19 10:06:57 -0800

    Commit for I can only go one way

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,7 +9,12 @@ void print_rev(char *s)
 
 	while (s[i] != '\0')
 		i++;
-	for (i = i - 1; i >= 0; i--)
-		_putchar(s[i]);
+	    i -= 1;
+
+	while (i >= 0)
+	    {
+		    _putchar(s[i]);
+		    i--;
+	    }
 	_putchar('\n');
 }

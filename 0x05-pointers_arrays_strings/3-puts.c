b75afd48472c5a88d62551b20c5500713cf7bc10
ebra244 <ebra244@gmail.com>
2022-12-19 08:35:05 -0800

    Commit for I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts - Function that prints a string
+ * @str: String to be printed
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}

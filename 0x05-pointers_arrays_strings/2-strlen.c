9ee4083aee8c5537bf2cfb549f9c3f544663c234
ebra244 <ebra244@gmail.com>
2022-12-19 08:11:13 -0800

    Commit for This report, by its very length, defends itself

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - Function that returns the length of a string
+ * @s: String to be checked
+ * Return: 1 (On Success)
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}

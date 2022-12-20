33fbf64086f80ad0bb85078800d426e4a643c260
ebra244 <ebra244@gmail.com>
2022-12-20 04:04:39 -0800

    Commit for Arrays are not pointers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,12 +11,12 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("a[i]: %d", a[i]);
+		printf("%d", a[i]);
 		if (i != n - 1)
 		{
-			_putchar(',');
-			_putchar(' ');
+			printf(",");
+			printf(" ");
 		}
 	}
-	_putchar('\n');
+	printf("\n");
 } 

8498b42e3c12322f5f33b077ed962bbffb182556
ebra244 <ebra244@gmail.com>
2022-12-21 09:19:15 -0800

    Commit for strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,17 +8,10 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-	int j = 0;
-
-	while (s1[i] != '\0')
-		i++;
-	while (s2[j] != '\0')
-		j++;
-	if (s1[i] > s2[j])
-		return (1);
-	else if (s1[i] == s2[j])
-		return (0);
-	else
-		return (-1);
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+		return (*s1 - *s2);
 }

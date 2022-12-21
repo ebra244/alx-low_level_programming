eb0071f3f8c4268c16ebfe83e4a5f87f2af2984c
ebra244 <ebra244@gmail.com>
2022-12-21 08:52:50 -0800

    Commit for strcpy

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,13 +4,20 @@
  * _strcmp - Function that compares two strings
  * @s1: First string
  * @s2: second string
- * return: 1 (if Success)
+ * Return: 1 (if S1 > S2), 0 (if S1 == S2), -1 (if S1 < S2)
  */
 int _strcmp(char *s1, char *s2)
 {
-	if (s1 > s2)
+	int i = 0;
+	int j = 0;
+
+	while (s1[i] != '\0')
+		i++;
+	while (s2[j] != '\0')
+		j++;
+	if (s1[i] > s2[j])
 		return (1);
-	else if (s2 == s1)
+	else if (s1[i] == s2[j])
 		return (0);
 	else
 		return (-1);

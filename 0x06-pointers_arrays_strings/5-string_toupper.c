f7672169aae91dc00351e37af00acf3447cc301d
ebra244 <ebra244@gmail.com>
2022-12-21 10:28:28 -0800

    Commit for Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ * string_toupper - Function that changes all lowercase letters to upper
+ * @a: String pointer to be changed
+ * Return: string pointer
+ */
+char *string_toupper(char *a)
+{
+	int i;
+
+	for (i = 0; i != '\0'; i++)
+	{
+		if (a[i] >= 'a' && a[i] <= 'z')
+			a[i] = a[i] - 32;
+	}
+	return (a);
+}

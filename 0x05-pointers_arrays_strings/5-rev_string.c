e960d62bac26d565fb02369fb3902e87290c4f72
ebra244 <ebra244@gmail.com>
2022-12-30 08:34:20 -0800

    Commit for A good engineer thinks in reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,16 +5,15 @@
  */
 void rev_string(char *s)
 {
-	int i, j;
+	int i;
 
 	i = 0;
 	while (s[i] != '\0')
 		i++;
-	j = i - 1;
-	while (j >= 0)
+	while (i >= 0)
 	{
-		_putchar(s[j]);
-		j--;
+		_putchar(s[i]);
+		i--;
 	}
 	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,12 +10,14 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
+	int j;
 
 	while (src[i] != '\0')
 	{
-		dest[i] = src[i];
+		for (j = 0; dest[j] != '\0'; j++)
+			dest[j] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
+	dest[j] = '\0';
 	return (dest);
 }

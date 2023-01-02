b84c520c2aea156ae7116e0e7a516e5b069cbd3e
ebra244 <ebra244@gmail.com>
2023-01-02 11:56:06 -0800

    Commit for strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strspn - Function that gets the length of a prefix substring
+ * @s:Pointer to the string
+ * @accept: Pointer to the prefix
+ * Return: Number of bytes (in the initial segment of s)
+ */
+unsigned
+int _strspn(char *s, char *accept)
+{
+	unsigned int bytes;
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				bytes++;
+				break;
+			}
+			else if (accept[i + 1] == '\0')
+				return (bytes);
+
+		}
+		s++;
+	}
+	return (bytes);
+}

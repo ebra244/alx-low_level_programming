68c8cb59ddebf7283252f8140f1bc7892a0e3deb
ebra244 <ebra244@gmail.com>
2023-01-05 09:27:07 -0800

    Commit for Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,6 +9,12 @@ int _sqrt_recursion(int n)
 {
 	return (_sqrt(n, 1));
 }
+/**
+ * _sqrt - Function which returns square root of i
+ * @n: Number whose square root returned
+ * @i: Square root of a number
+ * Return: 1 On success
+ */
 int _sqrt(int n, int i)
 {
 	int square = i * i;

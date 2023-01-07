349f736fde50f649958fce0cf94f25bd82dd6edc
ebra244 <ebra244@gmail.com>
2023-01-07 02:50:31 -0800

    Commit for Silence is argument carried out by other means

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 /**
  * main - Program that prints number of arguments passed to it
- * @agrc: Counter of number of arguments
+ * @argc: Counter of number of arguments
  * @argv: Array of string of a program
  * Return: 0 On success
  */
 int main(int argc, char *argv[])
 {
-	(void) argc;
+	(void) argv;
 	printf("%i\n", argc - 1);
 	return (0);
 }

c856ac282ff4fa63f14274d48c6711f10719a8a4
ebra244 <ebra244@gmail.com>
2023-01-07 04:53:30 -0800

    Commit for Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Program that multiplies two numbers
+ * @argc: Counter of Arguments
+ * @argv: Array of String of Arguments
+ * Return: 0 On success
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+
+	if (argc == 3)
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		printf("%d\n", num1 * num2);
+		return (0);
+	}
+	printf("error\n");
+	return (1);
+}

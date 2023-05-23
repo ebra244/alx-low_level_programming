b51ba33b33c2b79b6f3074ef8638bf723abf9672
ebra244 <ebra244@gmail.com>
2023-05-23 11:50:53 -0700

    8. I'm federal agent Jack Bauer, and today is the longest day of my life
    Write a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
    
    Prototype: void jack_bauer(void);
    You can listen to this soundtrack while coding :)

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * jack_bauer - Function that prints every minutes of a day
+ * Return: Always 0 (success)
+ */
+void jack_bauer(void)
+{
+	int m, n;
+
+	m = 0;
+	while (m < 24)
+	{
+		n = 0;
+
+		while (n < 60)
+		{
+			_putchar('m / 10');
+			_putchar('m % 10');
+			_putchar(':');
+			_putchar('n / 10');
+			_putchar('n % 10');
+			_putchar('\n');
+			n++;
+		}
+		m++;
+	}
+}

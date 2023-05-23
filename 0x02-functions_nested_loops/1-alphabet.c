fbce1491dfab0c952b59bca259b0268203eaeab2
ebra244 <ebra244@gmail.com>
2023-05-23 06:56:44 -0700

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game
    Write a function that prints the alphabet, in lowercase, followed by a new line.
    
    Prototype: void print_alphabet(void);
    You can only use _putchar twice in your code

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - Function that prints the alphabet in lowercase
+ * Return: Always 0 (success)
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar(c);
+}

531e40ee2f2ceef3f51ddb0299d252917b7c5ead
ebra244 <ebra244@gmail.com>
2023-05-30 12:02:57 -0700

    6. Half the lies they tell about me are not true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts2 - function that prints every other character of a string
+ * @str: Pointer of the string to be printed
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}

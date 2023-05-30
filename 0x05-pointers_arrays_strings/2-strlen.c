4528c624f2b99ec2d248036ef2a12b0bb1f3f787
ebra244 <ebra244@gmail.com>
2023-05-30 09:54:29 -0700

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _strlen - function that returns the length of a string
+ * @s: Pointer to the string
+ * Return: The length of string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}

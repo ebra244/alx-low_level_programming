b5de6beb576ea2ac35367de837bb27142e199766
ebra244 <ebra244@gmail.com>
2023-05-31 11:52:35 -0700

    Fix typo

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,19 +8,16 @@ char *cap_string(char *str)
 {
 	int i;
 
-	if (str[0] >= 'a' && str[0] <= 'z')
-		str[0] -= 32;
-	for (i = 1; str[i]; i++)
+	for (i = 0; str[i]; i++)
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
-		{
+		for (; !(str[i] >= 'a' && str[i] <= 'z'); i++)
+			;
 			if ((str[i - 1] == ' ') || (str[i - 1] == '\t') || (str[i - i1] == '\n')
 			|| (str[i - 1] == ',') || (str[i - 1] == '.') || (str[i - 1] == '!')
 			|| (str[i - 1] == '?') || (str[i - 1] == '"') || (str[i - 1] == '(')
 			|| (str[i - 1] == ')') || (str[i - 1] == '{') || (str[i - 1] == '}')
 			|| (str[i - 1] == ';'))
 				str[i] -= 32;
-		}
 	}
 	return (str);
 

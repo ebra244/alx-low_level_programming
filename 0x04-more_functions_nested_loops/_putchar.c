0e5da6468434af7e0219369fe1d763355db07bdc
ebra244 <ebra244@gmail.com>
2023-06-02 07:11:56 -0700

    Add code for 0-isupper

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * _putchar - Function that prints the charcter to std out
+ * @c: Character to be printed
+ * Return: Always 1 success
+ * -1 not success
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}

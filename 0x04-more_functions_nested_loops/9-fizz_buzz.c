76c12205a9112addf82f756c17f34714489e4c12
ebra244 <ebra244@gmail.com>
2023-06-03 11:34:28 -0700

    Fix typo for FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,11 +13,11 @@ int main(void)
 	{
 		if (i % 15 == 0)
 			printf("FizzBuzz ");
-		else if (i % 3 == 0 && i < 100)
+		else if (i % 3 == 0)
 			printf("Fizz ");
 		else if (i == 100)
 			printf("Buzz ");
-		else if (i % 5 == 0)
+		else if (i % 5 == 0 && i < 100)
 			printf("Buzz ");
 		else
 			printf("%d ", i);

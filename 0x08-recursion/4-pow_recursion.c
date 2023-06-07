2fab9f642f05507e11aa453fc495742b7d822e77
ebra244 <ebra244@gmail.com>
2023-06-07 10:20:40 -0700

    Add code for 4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Function that returns the power of a number
+ * @x: Number to be printed its power
+ * @y: Power of the number
+ * Return: Always -1 if power is negative
+ * 0 if number is 0
+ * positive if power is non negative
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}

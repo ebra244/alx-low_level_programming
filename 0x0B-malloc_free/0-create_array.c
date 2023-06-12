0cad307b432612919fa0e4739f47c01b2a5f9b62
ebra244 <ebra244@gmail.com>
2023-06-12 07:47:12 -0700

    Add code to create array of chars using malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * create_array - Function that creates an array of chars
+ * @size: Size of the array to be created
+ * @c: Characters to create the array
+ * Return: Pointer to the array if success
+ * NULL if not success
+ */
+char *create_array(unsigned int size, char c)
+{
+	int i;
+
+	if (size == 0)
+		return (NULL);
+	char *array = (char *)malloc(size * sizeof(char));
+
+	if (!array)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		array[i] = c;
+	return (array);
+}

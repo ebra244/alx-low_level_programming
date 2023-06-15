d772d287fade3991bcc81b86143d8546f53b3c54
ebra244 <ebra244@gmail.com>
2023-06-15 09:51:03 -0700

    Fix typo for 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,7 +14,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	ptr = malloc(nmemb * sizeof(unsigned int));
+	ptr = malloc(nmemb * size);
 
 	if (!ptr)
 		return (NULL);

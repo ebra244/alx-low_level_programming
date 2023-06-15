631e70fd67d3956986926b573bdb4eb12eb54283
ebra244 <ebra244@gmail.com>
2023-06-15 11:29:34 -0700

    Add code for 100-realloc.c

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _realloc - Function that reallocates amemory block
+ * @ptr: Pointer to the memory previously allocated
+ * @old_size: The size of ptr
+ * @new_size: The size of reallocated space
+ * Return: Pointer to the new space if success
+ * NULL if not success
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *hstr = (char *)ptr;
+	char *nstr;
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size > old_size || ptr == NULL)
+	{
+		nstr = malloc(new_size);
+		if (nstr == NULL)
+			return (NULL);
+		if (ptr != NULL)
+		{
+			for (i = 0; i < old_size; i++)
+				nstr[i] = hstr[i];
+		}
+	}
+	else if (new_size == old_size)
+		return (ptr);
+	free(ptr);
+	return (nstr);
+}

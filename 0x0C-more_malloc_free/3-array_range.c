49ec8b337c49ccb0770550985f1eb7f65cf8c8b3
ebra244 <ebra244@gmail.com>
2023-06-15 10:24:23 -0700

    Add code to afunction that creates an array of integers and put them sequncially from smallest to largest

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * array_range - Function that creates an array of integers
+ * @min: The smallest element of the array
+ * @max: The largest element of the array
+ * Return: Pointer to the newly created array if success
+ * NULL if fails
+ */
+int *array_range(int min, int max)
+{
+	char *array;
+	int i, j;
+
+	if (min > max)
+		return (NULL);
+	array = malloc((max - min + 1) * sizeof(int));
+	if (!array)
+		return (NULL);
+	for (i = min, j = 0; j < (max - min + 1); j++, i++)
+		array[j] = i;
+	return (array);
+}

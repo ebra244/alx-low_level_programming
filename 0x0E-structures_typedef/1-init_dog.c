0c1abc25eab3cc31a9a034afae81247c3e27f7e6
ebra244 <ebra244@gmail.com>
2023-06-19 13:23:02 -0700

    Add code to initalize a struct of type dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * init_dog - Function that initialize a struct
+ * @d: Pointer to the struct to be returned
+ * @name: First member of the struct
+ * @age: second member of the struct
+ * @owner: Last member of the struct
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}

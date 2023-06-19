6e0eb42064007038accde7731f3e49fbb77f3aff
ebra244 <ebra244@gmail.com>
2023-06-19 08:33:32 -0700

    Add code to print the name of a file followed by new line using macro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Begning of the program
+ * Return: Always 0 if success
+ * Non zero if not success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}

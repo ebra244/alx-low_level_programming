be05209c0d6438d9686d9060e753d251c3cbcef2
ebra244 <ebra244@gmail.com>
2023-06-21 09:53:30 -0700

    Add function that performs operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * op_add - Function that adds two integers
+ * @a: First operand
+ * @b: Second operand
+ * Return: Always The sum of two numbers if success
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - Function that subtracts two integers
+ * @a: First operand
+ * @b: Second operand
+ * Return: The difference of two numbers if success
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - Fuction that returns the product of two numbers
+ * @a: First operand
+ * @b: Second operand
+ * Return: The product of two numbers if success
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - Fuction that returns the division of two numbers
+ * @a: First operand
+ * @b: Second operand
+ * Return: The division of two numbers if success
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - Fuction that returns the modulo of two numbers
+ * @a: First operand
+ * @b: Second operand
+ * Return: The modulo of two numbers if success
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
+

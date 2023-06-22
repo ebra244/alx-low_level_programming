242626e25665ea3fce68a0b846b55490a4d60783
ebra244 <ebra244@gmail.com>
2023-06-22 08:03:19 -0700

    Fix typo of 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,12 +13,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(va, n);
 	for (i = 0; i < n; i++)
 	{
-		if (i < (n - 1) && separator != NULL)
-			printf("%d%s", va_arg(va, int), separator);
-		else
+		printf("%d", va_arg(va, int));
+		if ((i < n - 1) && separator != NULL)
 		{
-			printf("%d\n", va_arg(va, int));
+			printf("%s", separator);
 		}
 		va_end(va);
 	}
+	printf("\n");
 }

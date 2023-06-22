0ec4a6922391db54bdda405ef7b440bdc7dacb2c
ebra244 <ebra244@gmail.com>
2023-06-22 09:09:10 -0700

    Add code to create a function that prints a string

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - Function that prints strings
+ * @separator: The string to be printed between
+ * @n: Number of strings to be passed
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list vap;
+
+	va_start(vap, n);
+	for (i = 0; i < n; i++)
+	{
+		char *str = va_arg(vap, char *);
+
+		printf("%s", str);
+		if ((i < n - 1) && separator != NULL)
+			printf("%s", separator);
+		va_end(vap);
+	}
+	printf("\n");
+}

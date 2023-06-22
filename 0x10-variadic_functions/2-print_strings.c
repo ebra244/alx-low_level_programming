3ab101fe380f1dc08e4c09102098d58bc58da586
ebra244 <ebra244@gmail.com>
2023-06-22 09:19:53 -0700

    Fix typo for 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,7 +16,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		char *str = va_arg(vap, char *);
 
 		if (str == NULL)
-			printf("%s", (nil));
+			printf("(nil)");
 		printf("%s", str);
 		if ((i < n - 1) && separator != NULL)
 			printf("%s", separator);

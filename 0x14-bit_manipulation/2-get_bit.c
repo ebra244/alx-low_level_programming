910df43ed005b5db643c5b1d9c67ff76e4574763
ebra244 <ebra244@gmail.com>
2023-07-06 11:12:32 -0700

    Add code which returns the value of abit at the given index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_bit - Function that prints the value of a bit at 
+ * given index
+ * @n: Number to be tested
+ * @index: Index to be returned its value
+ * Return: The value of a bit if success
+ * Otherwise -1
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+	n = n >> index;
+	return (n & 1);
+
+}

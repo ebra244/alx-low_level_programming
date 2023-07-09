c581c853aa2eed319f587015adb9f8c45e91ea59
ebra244 <ebra244@gmail.com>
2023-07-09 09:33:17 -0700

    Add code which returns the number of elements of a list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - Function that returns the number of elements of a list
+ * @h: Pointer to the head of alist
+ * Returns: The number of elements if success
+ * Otherwise 0
+ */
+size_t list_len(const list_t *h)
+{
+	unsigned int count = 0;
+
+	if (!h)
+		return (0);
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
